73290eec1ae28bf24ea5f04b41797ae5f184a2e8
eduardo-072 <eduardosilvaaa072@gmail.com>
2023-11-27 16:51:41 -0300

    tarefa realizada com atraso

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿﻿Console.Write("Números.....: ");
+int quantidade = Convert.ToInt32(Console.ReadLine());
+
+decimal numeroDigitado;
+int contador = 0;
+
+decimal soma = 0;
+decimal NumeroMaior = 999999.99M;
+decimal NumeroMenor = 999999.99M;
+
+while (contador < quantidade)
+{
+    contador += 1;
+    Console.Write($"Número {contador}.....: ");
+    numeroDigitado = Convert.ToDecimal(Console.ReadLine());
+    soma = soma + numeroDigitado;
+
+    if (contador == 1)
+    {
+        NumeroMaior = numeroDigitado;
+        NumeroMenor = numeroDigitado;
+    }
+
+    if (numeroDigitado > NumeroMaior)
+    {
+        NumeroMaior = numeroDigitado;
+    }
+
+    if (numeroDigitado <NumeroMenor)
+    {
+        NumeroMenor = numeroDigitado;
+    }
+}
+
+if (contador > 0)
+{
+    decimal media = soma / quantidade;
+
+    Console.WriteLine($"\nSoma...: {soma:N2}");
+    Console.WriteLine($"Média....: {media:N2}");
+    Console.WriteLine($"Menor....: {NumeroMenor:N2}");
+    Console.WriteLine($"Maior....: {NumeroMaior:N2}");
+}
